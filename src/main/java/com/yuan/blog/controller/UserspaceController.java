b803eb21a1dc450aa0448e6282166b7b6dc82ee6
milo <qcmzby@163.com>
2018-10-21 19:06:40 +0800

    修改个人信息 修改头像 使用mongodb存储头像文件。

--- src/main/java/com/yuan/blog/controller/UserspaceController.java
+++ src/main/java/com/yuan/blog/controller/UserspaceController.java
@@ -1,10 +1,20 @@
 package com.yuan.blog.controller;
 
+import com.yuan.blog.domain.User;
+import com.yuan.blog.service.UserService;
+import com.yuan.blog.vo.Response;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 用户主页空间控制器.
@@ -12,19 +22,102 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/u")
 public class UserspaceController {
- 
+
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserDetailsService userDetailsService;
+	@Value("$(file.server.url)")
+	private String fileServerUrl;
+
+	/**
+	 *  获取用户配置
+	 * @param username
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/{username}/profile")
+	@PreAuthorize("authentication.name.equals(#username)")
+	public ModelAndView profile(@PathVariable("username") String username, Model model) {
+		User  user = (User)userDetailsService.loadUserByUsername(username);
+		model.addAttribute("user", user);
+		model.addAttribute("fileServerUrl", fileServerUrl);
+		return new ModelAndView("/userspace/profile", "userModel", model);
+	}
+
+	/**
+	 * 保存个人设置
+	 */
+	@PostMapping("/{username}/profile")
+	@PreAuthorize("authentication.name.equals(#username)")
+	public String saveProfile(@PathVariable("username") String username,User user) {
+		User originalUser = userService.getUserById(user.getId());
+		originalUser.setEmail(user.getEmail());
+		originalUser.setName(user.getName());
+
+		// 判断密码是否做了变更
+		String rawPassword = originalUser.getPassword();
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		String encodePasswd = encoder.encode(user.getPassword());
+		boolean isMatch = encoder.matches(rawPassword, encodePasswd);
+		if (!isMatch) {
+			originalUser.setEncodePassword(user.getPassword());
+		}
+
+		userService.saveOrUpdateUser(originalUser);
+		return "redirect:/u/" + username + "/profile";
+	}
+
+	/**
+	 * 获取编辑头像的界面
+	 */
+	@GetMapping("/{username}/avatar")
+	@PreAuthorize("authentication.name.equals(#username)")
+	public ModelAndView avatar(@PathVariable("username") String username, Model model) {
+		User  user = (User)userDetailsService.loadUserByUsername(username);
+		model.addAttribute("user", user);
+		return new ModelAndView("/userspace/avatar", "userModel", model);
+	}
+
+	/**
+	 * 保存头像
+	 */
+	@PostMapping("/{username}/avatar")
+	@PreAuthorize("authentication.name.equals(#username)")
+	public ResponseEntity<Response> saveAvatar(@PathVariable("username") String username, @RequestBody User user) {
+		String avatarUrl = user.getAvatar();
+
+		User originalUser = userService.getUserById(user.getId());
+		// localhost:8081/view/5bcc58649e76f8d8e4f9cd90
+		// 存的是mongodb 的id
+		originalUser.setAvatar(avatarUrl);
+		userService.saveOrUpdateUser(originalUser);
+		return ResponseEntity.ok().body(new Response(true, "处理成功", avatarUrl));
+	}
+
+	// 根据用户名 获取用户信息
 	@GetMapping("/{username}")
-	public String userSpace(@PathVariable("username") String username) {
-		System.out.println("username" + username);
-		return "/userspace/u";
+	public String userSpace(@PathVariable("username") String username, Model model) {
+		//todo 哪里掉这里了？？？
+		if(StringUtils.isEmpty(username)){
+			username = "milo";
+		}
+		User  user = (User)userDetailsService.loadUserByUsername(username);
+		model.addAttribute("user", user);
+		return "redirect:/u/" + username + "/blogs";
 	}
  
 	@GetMapping("/{username}/blogs")
 	public String listBlogsByOrder(@PathVariable("username") String username,
-			@RequestParam(value="order",required=false,defaultValue="new") String order,
-			@RequestParam(value="category",required=false ) Long category,
-			@RequestParam(value="keyword",required=false ) String keyword) {
-		
+								   @RequestParam(value="order",required=false,defaultValue="new") String order,
+								   @RequestParam(value="category",required=false ) Long category,
+								   @RequestParam(value="keyword",required=false,defaultValue="" ) String keyword,
+								   @RequestParam(value="async",required=false) boolean async,
+								   @RequestParam(value="pageIndex",required=false,defaultValue="0") int pageIndex,
+								   @RequestParam(value="pageSize",required=false,defaultValue="10") int pageSize,
+								   Model model) {
+		User  user = (User)userDetailsService.loadUserByUsername(username);
+		model.addAttribute("user", user);
 		if (category != null) {
 			
 			System.out.print("category:" +category );
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 /*  项目启动 自动执行的数据库脚本 ==> 密码是 123456*/
 
-INSERT INTO user (id, username, password, name, email) VALUES (1, 'admin', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', '老卫', 'i@waylau.com');
-INSERT INTO user (id, username, password, name, email)  VALUES (2, 'waylau', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi', 'Way Lau', 'waylau@waylau.com');
+INSERT INTO user (id, username, password, name, email) VALUES (1, 'milo', '$2a$10$mu.Ezc2d7yfAPxf4oOqptuJFa9VCr4VXbXZrioz2b8cDdGH/cmAPm', 'milo', 'i@waylau.com');
+INSERT INTO user (id, username, password, name, email)  VALUES (2, 'venna', '$2a$10$mu.Ezc2d7yfAPxf4oOqptuJFa9VCr4VXbXZrioz2b8cDdGH/cmAPm', 'venna', 'waylau@waylau.com');
 
 INSERT INTO authority (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO authority (id, name) VALUES (2, 'ROLE_USER');
