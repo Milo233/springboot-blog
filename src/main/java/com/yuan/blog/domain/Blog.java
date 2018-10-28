3da27f69ee3e440c36f4c3634ba838059e2ef2dc
milo <qcmzby@163.com>
2018-10-28 17:37:18 +0800

    博客评论的实现

--- src/main/java/com/yuan/blog/domain/Blog.java
+++ src/main/java/com/yuan/blog/domain/Blog.java
@@ -1,12 +1,10 @@
 package com.yuan.blog.domain;
 
 import com.github.rjeschke.txtmark.Processor;
-import com.sun.org.apache.xml.internal.resolver.Catalog;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
-import javax.xml.stream.events.Comment;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
@@ -55,14 +53,19 @@ public class Blog implements Serializable {
 	@org.hibernate.annotations.CreationTimestamp  // 由数据库自动创建时间
 	private Timestamp createTime;
 
-	@Column(name="reading")
-	private Long reading = 0L; // 访问量、阅读量
+	@Column(name="readSize")
+	private Integer readSize = 0; // 访问量、阅读量
 
-	@Column(name="comments")
-	private Long comments = 0L;  // 评论量
+	@Column(name="commentSize")
+	private Integer commentSize = 0;  // 评论量
 
-	@Column(name="likes")
-	private Long likes = 0L;  // 点赞量
+	@Column(name="likeSize")
+	private Integer likeSize = 0;  // 点赞量
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "blog_comment", joinColumns = @JoinColumn(name = "blog_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "comment_id", referencedColumnName = "id"))
+	private List<Comment> comments;
 
 	protected Blog() {
 		// TODO Auto-generated constructor stub
@@ -120,23 +123,58 @@ public class Blog implements Serializable {
 		return htmlContent;
 	}
 
-	public Long getComments() {
-		return comments;
-	}
-	public void setComments(Long comments) {
-		this.comments = comments;
-	}
-	public Long getLikes() {
-		return likes;
-	}
-	public void setLikes(Long likes) {
-		this.likes = likes;
-	}
-	public Long getReading() {
-		return reading;
-	}
-	public void setReading(Long reading) {
-		this.reading = reading;
+	public Integer getReadSize() {
+		return readSize;
 	}
 
+	public void setReadSize(Integer readSize) {
+		this.readSize = readSize;
+	}
+
+	public Integer getCommentSize() {
+		return commentSize;
+	}
+
+	public void setCommentSize(Integer commentSize) {
+		this.commentSize = commentSize;
+	}
+
+	public Integer getLikeSize() {
+		return likeSize;
+	}
+
+	public void setLikeSize(Integer likeSize) {
+		this.likeSize = likeSize;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+		this.commentSize = comments.size();
+	}
+
+	/**
+	 * 添加评论
+	 * @param comment
+	 */
+	public void addComment(Comment comment) {
+		this.comments.add(comment);
+		this.commentSize = this.comments.size();
+	}
+
+	/**
+	 * 删除评论
+	 */
+	public void removeComment(Long commentId) {
+		for (int index=0; index < this.comments.size(); index ++ ) {
+			if (comments.get(index).getId() == commentId) {
+				this.comments.remove(index);
+				break;
+			}
+		}
+		this.commentSize = this.comments.size();
+	}
 }
---
+++ src/main/java/com/yuan/blog/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.yuan.blog.repository;
+
+import com.yuan.blog.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Comment 仓库.
+ */
+public interface CommentRepository extends JpaRepository<Comment, Long>{
+ 
+}
