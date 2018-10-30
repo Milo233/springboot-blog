18f70927d833d29ca0c6eee3cf43917a31c38236
milo <qcmzby@163.com>
2018-10-30 08:10:00 +0800

    增加全文检索 未完成

---
+++ src/main/java/com/yuan/blog/repository/EsBlogRepository.java
@@ -0,0 +1,34 @@
+package com.yuan.blog.repository;
+
+import com.yuan.blog.domain.EsBlog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * EsBlog Repository接口.
+ * 
+ * @since 1.0.0 2017年6月8日
+ * @author <a href="https://waylau.com">Way Lau</a> 
+ */
+public interface EsBlogRepository extends ElasticsearchRepository<EsBlog, String> {
+	
+    /**
+     * 模糊查询(去重)
+     * @param title
+     * @param summary
+     * @param content
+     * @param tags
+     * @param pageable
+     * @return
+     */
+    Page<EsBlog> findByTitleContainingOrSummaryContainingOrContentContainingOrTagsContaining(
+            String title, String summary, String content, String tags, Pageable pageable);
+
+    /**
+     * 根据 Blog 的id 查询 EsBlog
+     * @param blogId
+     * @return
+     */
+    EsBlog findByBlogId(Long blogId);
+}
--- src/main/java/com/yuan/blog/service/UserService.java
+++ src/main/java/com/yuan/blog/service/UserService.java
@@ -4,6 +4,7 @@ import com.yuan.blog.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface UserService {
@@ -43,4 +44,11 @@ public interface UserService {
      * 根据用户名进行分页模糊查询
      */
     Page<User> listUsersByNameLike(String name, Pageable pageable);
+
+    /**
+     * 根据用户名集合，查询用户详细信息列表
+     * @param usernames
+     * @return
+     */
+    List<User> listUsersByUsernames(Collection<String> usernames);
 }
