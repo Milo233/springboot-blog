d239ee6d65ebd0d102e4f3ddba2f6ceeede98733
zhangyuan <748561384@qq.com>
2018-11-16 19:32:54 +0800

    模糊查询加上博客内容。。个人数据量小，查询不会很慢

--- src/main/java/com/yuan/blog/repository/BlogRepository.java
+++ src/main/java/com/yuan/blog/repository/BlogRepository.java
@@ -13,32 +13,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BlogRepository extends JpaRepository<Blog, Long>{
 
 	/**
-	 * 根据用户名分页查询博客列表
-	 * @param user
-	 * @param title
-	 * @param pageable
-	 * @return
-	 */
-	Page<Blog> findByUserAndTitleLikeOrderByCreateTimeDesc(User user, String title, Pageable pageable);
-
-	/**
 	 * 根据用户 && (博客标题 || 博客标签) 分页查询博客列表
 	 */
-	Page<Blog> findByUserAndTitleLikeOrUserAndTagsLike(User user, String title, User user2, String tag, Pageable pageable);
+	Page<Blog> findByUserAndTitleLikeOrUserAndTagsLikeOrUserAndContentLike(User user, String title, User user2, String tag,
+																		   User user3, String title3,Pageable pageable);
 
 	/**
 	 * (博客标题 || 博客标签) 分页查询博客列表 不含用户
 	 */
-	Page<Blog> findByTitleLikeOrTagsLike(String title,String tag, Pageable pageable);
-
-	/**
-	 * 根据用户名分页查询博客列表
-	 * @param user
-	 * @param title
-	 * @param pageable
-	 * @return
-	 */
-	Page<Blog> findByUserAndTitleLike(User user, String title, Pageable pageable);
+	Page<Blog> findByTitleLikeOrTagsLikeOrContentLike(String title,String tag, String title3,Pageable pageable);
 
 
 	/**
