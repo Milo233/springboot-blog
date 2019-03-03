cfeb7594c895b1a1389d6afecfbcda25430c9cf0
milo <qcmzby@163.com>
2019-03-03 08:51:15 +0800

    测试 cron

--- src/main/java/com/yuan/blog/timer/ScheduledTask.java
+++ src/main/java/com/yuan/blog/timer/ScheduledTask.java
@@ -12,8 +12,8 @@ public class ScheduledTask {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
 
-//    @Scheduled(cron="0 0/1 * * * ?") // 每分钟执行一次
-    @Scheduled(fixedRate = 4 * 60 * 60 * 1000 ) // 4h 一次
+    @Scheduled(cron="0 0 * * *  ?") // 每小时 整点 0分执行一次
+//    @Scheduled(fixedRate = 4 * 60 * 60 * 1000 ) // 4h 一次 项目第一次启动时就会执行一次
     public void testOne() {
         log.info("定时任务 4h 执行一次");
     }
