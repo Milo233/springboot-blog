20c70d94b373890ac1edaef04c2cc25c29cf8cd9
milo <qcmzby@163.com>
2019-03-04 23:25:29 +0800

    修改定时任务为cron

--- src/main/java/com/yuan/blog/timer/ScheduledTask.java
+++ src/main/java/com/yuan/blog/timer/ScheduledTask.java
@@ -9,27 +9,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
-import java.util.Date;
 
-@EnableScheduling
 @Component
 public class ScheduledTask {
 
     @Autowired
     private SystemLogService systemLogService;
 
-    private Date LAST_DATE = null;
-
     private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
 
-//    @Scheduled(cron="0 0 * * *  ?") // 每小时 整点 0分执行一次
-    @Scheduled(fixedRate = 4 * 60 * 60 * 1000 ) // 24h 一次 项目第一次启动时就会执行一次
+    @Scheduled(cron="0 30 /6 * * ? ") // 每6个小时 执行一次
+//    @Scheduled(fixedRate = 4 * 60 * 60 * 1000 ) // 24h 一次 项目第一次启动时就会执行 大部分时候启动时会执行两次。。还是用cron的好
     public void deleteSystemLog() {
-        // todo 有时刚启动项目时会执行两次。。
-        if (LAST_DATE != null && (new Date().getTime() - LAST_DATE.getTime() < 4 * 60 * 60 * 1000)){
-            log.info("定时重复啦！！！");
-        }
-        log.info("定时任务 每天执行一次 删除3天以前的日志(留下id小于、等于50的)");
+        log.info("cron 定时任务 每天执行4一次 删除3天以前的日志(留下id小于、等于50的)");
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DATE,-3);
         systemLogService.deleteLogsBefore(cal.getTime());
