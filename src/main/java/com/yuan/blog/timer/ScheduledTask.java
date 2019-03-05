b027afeaf57b21d91ffd3e74b85c8cfe8ac2cae2
milo <qcmzby@163.com>
2019-03-05 23:56:51 +0800

    加上注解

--- src/main/java/com/yuan/blog/timer/ScheduledTask.java
+++ src/main/java/com/yuan/blog/timer/ScheduledTask.java
@@ -8,20 +8,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Calendar;
 
+@EnableScheduling
 @Component
 public class ScheduledTask {
 
-    @Autowired
+    @Resource
     private SystemLogService systemLogService;
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
 
-    @Scheduled(cron="0 30 /6 * * ? ") // 每6个小时 执行一次
-//    @Scheduled(fixedRate = 4 * 60 * 60 * 1000 ) // 24h 一次 项目第一次启动时就会执行 大部分时候启动时会执行两次。。还是用cron的好
+    //@Scheduled(fixedRate = 4 * 60 * 60 * 1000 ) // 24h 一次 项目第一次启动时就会执行 大部分时候启动时会执行两次。。还是用cron的好
+    @Scheduled(cron="0 59 23 * * ? ")
     public void deleteSystemLog() {
-        log.info("cron 定时任务 每天执行4一次 删除3天以前的日志(留下id小于、等于50的)");
+        log.info("cron 定时任务 删除3天以前的日志(留下id小于、等于50的)");
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DATE,-3);
         systemLogService.deleteLogsBefore(cal.getTime());
